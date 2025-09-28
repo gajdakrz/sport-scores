345e564bacb54dcf12a3abf1926b39db6894100c
Krzysztof Gajda <gajda.krzysztof@gmail.com>
2025-09-28 12:12:25 +0200

    usunięcie zbędnego use

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -15,7 +15,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use OpenApi\Attributes as OA;
